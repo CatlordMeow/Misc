5cd3583f2ad520a02fb5f478a0830bd36a3bfcec
Catlord Meow! <impishpoke@gmail.com>
2016-07-19 14:22:34 +0300

    Update vbb-utils.js

--- vbb-utils.js
+++ vbb-utils.js
@@ -1,5 +1,5 @@
 // @name vbb-utils
-// @version 1.2
+// @version 1.3
 // eslint-disable-next-line
 // @description Helper script for vBulletin 4.2 forums. Bells and whistlers.
 // @author Catlord Meow!, http://sovserv.su/member.php?u=237
